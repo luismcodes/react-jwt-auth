356d6409cc3d5b23c6ff0974629981ef3f2cae90
Luis Morales Bernal <luismcodes@gmail.com>
2022-11-24 17:15:14 -0600

    Login Page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from "./serviceWorker";
 
 import './index.css'
 
@@ -13,5 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
-
-serviceWorker.unregister();
